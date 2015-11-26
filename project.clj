a575c64a94910eff9c2d067cdba9ef3be2d522d9
Felipe Gerard <fgerard@interware.com.mx>
2015-11-26 10:51:21 -0600

    src

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clojure101 "0.1.0-SNAPSHOT"
+(defproject curso101 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                           [org.clojure/core.match "0.3.0-alpha4"]
+                          [net.mikera/core.matrix "0.46.0"]
                           [incanter "[1.2.2,)"]
                           [datascript "0.13.2"]])
