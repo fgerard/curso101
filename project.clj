e7946198e43ad432e01fe9037667ff3c02f807ab
Felipe Gerard <fgerard@interware.com.mx>
2015-11-26 10:10:37 -0600

    solo project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clojure101 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                          [org.clojure/core.match "0.3.0-alpha4"]
+                          [incanter "[1.2.2,)"]
+                          [datascript "0.13.2"]])
